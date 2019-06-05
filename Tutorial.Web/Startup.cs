65c55c4d06d9a8d192c6323d30bf23880b030dfa
starpan123 <m13110032289@163.com>
2019-06-05 14:39:48 +0800

    修改

--- Tutorial.Web/Startup.cs
+++ Tutorial.Web/Startup.cs
@@ -27,7 +27,7 @@ namespace Tutorial.Web
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!123456");
             });
         }
     }
